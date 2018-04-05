50e620e10a64f881ba134c0969189a3c010367bc
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-04-04 20:15:39 -0600

    Add captureUser prop to FormsContainer
    Passed down to presentational component as part of handleOnSubmit method of FormsContainer

--- src/components/UserForm/index.test.js
+++ src/components/UserForm/index.test.js
@@ -1,23 +1,38 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import UserForm from './index';
 
 describe('UserForm', () => {
   let wrapper;
   const mockHandleOnChange = jest.fn();
+  const mockHandleOnSubmit = jest.fn();
 
   beforeEach(() => {
     wrapper = shallow(
-      <UserForm 
+      <UserForm
         name=''
         email=''
         password=''
-        routeId="login"
+        routeId='login'
         handleOnChange={mockHandleOnChange}
+        handleOnSubmit={mockHandleOnSubmit}
       />
     );
   });
   it('should match a snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match a snapshot with the route "signup"', () => {
+    wrapper = shallow(
+      <UserForm
+        name=''
+        email=''
+        password=''
+        routeId='signup'
+        handleOnChange={mockHandleOnChange}
+        handleOnSubmit={mockHandleOnSubmit}
+      />
+    );
   });
 });
