b8964a60026d04fcb0ad506a4135b82c2c7beb36
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-04-09 12:59:04 -0600

    Add style to report button on landing page

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -3,13 +3,20 @@ import Welcome from '../Welcome';
 import {Link} from 'react-router-dom';
 import Search from '../Search';
 import PropTypes from 'prop-types';
+import './style.css';
 
 const LandingPage = ({username}) => {
   return (
     <main className='landing-page'>
       <Welcome username={username} />
       <Search />
-      <Link to='/report'>Report Found Item</Link> 
+      {
+        username &&
+        <div>
+          <h2 className='or'>Or</h2>
+          <Link className='report-link' to='/report'>Report Found Item</Link> 
+        </div>
+      }
     </main>
   );
 };
